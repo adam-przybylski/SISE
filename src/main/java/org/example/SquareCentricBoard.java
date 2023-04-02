f43c79b8a51045d351b2506796c6ac76794db0ba
adam-przybylski <adamprzybylski789@gmail.com>
2023-04-02 14:41:19 +0200

    Move function, getNeighbors func and test template

--- src/main/java/org/example/SquareCentricBoard.java
+++ src/main/java/org/example/SquareCentricBoard.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SquareCentricBoard implements Board {
@@ -15,7 +16,18 @@ public class SquareCentricBoard implements Board {
 
     @Override
     public List<MoveTurple> getNeighbors() {
-        return null;
+        List<MoveTurple> result = new ArrayList<>();
+
+        for(Move move : Move.values()) {
+            try {
+                SquareCentricBoard newBoard = new SquareCentricBoard(board);
+                newBoard.move(move);
+                result.add(new MoveTurple(newBoard, move));
+            } catch (WrongMoveException e) {
+                // do nothing
+            }
+        }
+        return result;
     }
 
     @Override
@@ -23,15 +35,15 @@ public class SquareCentricBoard implements Board {
         if (board[rows - 1][columns - 1] != 0) {
             return false;
         }
-        for (int i = 0; i < rows-1; i++) {
+        for (int i = 0; i < rows - 1; i++) {
             for (int j = 0; j < columns; j++) {
                 if (board[i][j] != i * columns + j + 1) {
                     return false;
                 }
             }
         }
-        for (int i = 0; i < columns-1; i++) {
-            if (board[rows-1][i] != (rows-1) * columns + i + 1) {
+        for (int i = 0; i < columns - 1; i++) {
+            if (board[rows - 1][i] != (rows - 1) * columns + i + 1) {
                 return false;
             }
         }
@@ -40,6 +52,46 @@ public class SquareCentricBoard implements Board {
 
     @Override
     public void move(Move move) throws WrongMoveException {
+        int zeroRow = 0;
+        int zeroCol = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (board[i][j] == 0) {
+                    zeroRow = i;
+                    zeroCol = j;
+                }
+            }
+        }
+        switch (move) {
+            case UP -> {
+                if (zeroRow == 0) {
+                    throw new WrongMoveException();
+                }
+                board[zeroRow][zeroCol] = board[zeroRow - 1][zeroCol];
+                board[zeroRow - 1][zeroCol] = 0;
+            }
+            case DOWN -> {
+                if (zeroRow == rows - 1) {
+                    throw new WrongMoveException();
+                }
+                board[zeroRow][zeroCol] = board[zeroRow + 1][zeroCol];
+                board[zeroRow + 1][zeroCol] = 0;
+            }
+            case LEFT -> {
+                if (zeroCol == 0) {
+                    throw new WrongMoveException();
+                }
+                board[zeroRow][zeroCol] = board[zeroRow][zeroCol - 1];
+                board[zeroRow][zeroCol - 1] = 0;
+            }
+            case RIGHT -> {
+                if (zeroCol == columns - 1) {
+                    throw new WrongMoveException();
+                }
+                board[zeroRow][zeroCol] = board[zeroRow][zeroCol + 1];
+                board[zeroRow][zeroCol + 1] = 0;
+            }
+        }
 
     }
 }
---
+++ src/test/java/org/example/SquareCentricBoardTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.Test;
+
+public class SquareCentricBoardTest {
+
+    @Test
+    public void constructorTest() {
+
+    }
+
+    @Test
+    public void getNeighborsTest() {
+
+    }
+
+    @Test
+    public void isGoalTest() {
+
+    }
+
+    @Test
+    public void moveTest() {
+
+    }
+}
